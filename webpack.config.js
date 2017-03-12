19a268bae0064e60dc51a0b9bdf866c14be786d8
Kacper Pikulski <pikulak1@gmail.com>
2017-03-12 17:16:10 +0100

    First run

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var webpack = require('webpack');
+ 
+module.exports = {
+  entry: './frontend/dev/jsx/main.js',
+  output: { path: "./frontend/static/scripts/js/", filename: 'app.js' },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2016', 'react']
+        }
+      }
+    ]
+  },
+};
